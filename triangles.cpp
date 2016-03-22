9f11c8c182553c0c829eb2f04d893ce97587cdd2
Vagan Grigoryan <vagangrig@gmail.com>
2016-03-21 22:20:45 -0400

    First commit.

---
+++ triangles.cpp
@@ -0,0 +1,58 @@
+/*
+ * CSc103 Project 4: Triangles
+ * See readme.html for details.
+ * Please list all references you made use of in order to complete the
+ * assignment: your classmates, websites, etc.  Aside from the lecture notes
+ * and the book, please list everything.  And remember- citing a source does
+ * NOT mean it is okay to COPY THAT SOURCE.  What you submit here **MUST BE
+ * YOUR OWN WORK**.
+ * References:
+ *
+ *
+ * Finally, please indicate approximately how many hours you spent on this:
+ * #hours: 
+ */
+
+#include "triangles.h" // import the prototypes for our triangle class.
+#include <iostream>
+using std::cout;
+#include <vector>
+using std::vector;
+#include <algorithm>
+using std::sort;
+
+// note the "triangle::" part.  We need to specify the function's
+// FULL name to avoid confusion.  Else, the compiler will think we
+// are just defining a new function called "perimeter"
+unsigned long triangle::perimeter() {
+	return s1+s2+s3;
+}
+
+unsigned long triangle::area() {
+	// TODO: write this function.
+	// Note: why is it okay to return an integer here?  Recall that
+	// all of our triangles have integer sides, and are right triangles...
+	return 0;
+}
+
+void triangle::print() {
+	cout << "[" << s1 << "," << s2 << "," << s3 << "]";
+}
+
+bool congruent(triangle t1, triangle t2) {
+	// TODO: write this function.
+	return false;
+}
+
+bool similar(triangle t1, triangle t2) {
+	// TODO: write this function.
+	return false;
+}
+
+vector<triangle> findRightTriangles(unsigned long l, unsigned long h) {
+	// TODO: find all the right triangles with integer sides,
+	// subject to the perimeter bigger than l and less than h
+	vector<triangle> retval; // storage for return value.
+	return retval;
+}
+
